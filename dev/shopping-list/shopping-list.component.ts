8bf6f5bfa24459067d33eaa5d39edff04226dee7
Abhimanyu310 <abhip310@gmail.com>
2016-07-07 13:04:27 +0530

    Add and edit item feature added

--- dev/shopping-list/shopping-list.component.ts
+++ dev/shopping-list/shopping-list.component.ts
@@ -1,18 +1,39 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ShoppingListEditComponent} from "./shopping-list-edit.component";
+import {Ingredient} from "../shared/ingredient";
+import {ShoppingListService} from "../shared/shopping-list.service";
 
 @Component({
     selector: 'my-shopping-list',
     template: `
-        <h1>Shopping List</h1>
-        <div>Edit Section</div>
+        <h1>Shopping Lis1t</h1>
+        <my-shopping-list-edit [ingredient]="selectedItem"></my-shopping-list-edit>
         <div class="list">
-            <button class="btn">Add new Item</button>
+            <button class="btn" (click)="onAddItem()">Add new Item</button>
             <ul>
-                <li>List Items...</li>
+                <li *ngFor="let item of shoppingList" (click)="onSelectItem(item)">{{item.name}} ({{item.amount}})</li>
             </ul>
         </div>
-    `
+    `,
+    directives: [ShoppingListEditComponent],
+    providers: [ShoppingListService]
 })
-export class ShoppingListComponent{
+export class ShoppingListComponent implements OnInit {
+    shoppingList: Ingredient[];
+    selectedItem: Ingredient = null;
+
+    constructor(private _shoppingListService: ShoppingListService){}
+
+    ngOnInit(){
+        this.shoppingList = this._shoppingListService.getAllItems();
+    }
+
+    onSelectItem(item: Ingredient){
+        this.selectedItem = item;
+    }
+
+    onAddItem(){
+        this.selectedItem = null;
+    }
 
 }
