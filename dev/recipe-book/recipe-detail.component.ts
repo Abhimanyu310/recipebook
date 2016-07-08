2ab07bf27d03e2be916775313ce673dde3b80b4f
Abhimanyu310 <abhip310@gmail.com>
2016-07-08 17:17:43 +0530

    Can edit recipes... need option to save but

--- dev/recipe-book/recipe-detail.component.ts
+++ dev/recipe-book/recipe-detail.component.ts
@@ -1,15 +1,19 @@
 import {Component, OnInit} from "@angular/core";
 import {Recipe} from "../shared/recipe";
-import { RouteSegment } from "@angular/router"
+import { RouteSegment, Router, OnActivate } from "@angular/router";
 import {RecipeService} from "./recipe.service";
+import {RecipeEditComponent} from "./recipe-edit.component";
+import {ShoppingListService} from "../shared/shopping-list.service";
 
 @Component({
-    templateUrl: 'templates/recipes/recipe-detail.tpl.html'
+    templateUrl: 'templates/recipes/recipe-detail.tpl.html',
+    providers: [ShoppingListService],
+    directives: [RecipeEditComponent]
 })
-export class RecipeDetailComponent implements OnInit{
+export class RecipeDetailComponent implements OnInit, OnActivate{
     recipe: Recipe;
     private _recipeIndex: string;
-    constructor(private _routeSegment: RouteSegment, private _recipeService: RecipeService){}
+    constructor(private _routeSegment: RouteSegment, private _recipeService: RecipeService, private _router: Router){}
 
     routerOnActivate(curr:RouteSegment, prev?:RouteSegment):void {
         let itemIndex = curr.getParam('id');
@@ -20,4 +24,7 @@ export class RecipeDetailComponent implements OnInit{
         this.recipe = this._recipeService.getRecipe(this._recipeIndex !== null ? +this._recipeIndex : null) || null;
     }
 
+    onEdit(){
+        this._router.navigate(['/recipe/edit', this._recipeIndex]);
+    }
 }
--- dev/recipe-book/recipe-edit.component.ts
+++ dev/recipe-book/recipe-edit.component.ts
@@ -1,8 +1,74 @@
 import {Component, OnInit} from '@angular/core';
+import {ControlGroup} from '@angular/common';
+import { RouteSegment, Router } from "@angular/router";
+import {ControlGroup, Control, ControlArray, Validators, FormBuilder} from "@angular/common";
+import {RecipeService} from "./recipe.service";
+import {Recipe} from "../shared/recipe";
 
 @Component({
     templateUrl: '/templates/recipes/recipe-edit.tpl.html'
 })
-export class RecipeEditComponent{
+export class RecipeEditComponent implements OnInit{
+    myForm: ControlGroup;
+    recipe: Recipe;
+    private _editMode = 'create';
+    private _recipeIndex: number;
 
+    constructor(private _routeSegment:RouteSegment, private _recipeService:RecipeService,
+                private _formBuilder:FormBuilder, private _router: Router) {
+    }
+
+    ngOnInit() {
+        if (this._routeSegment.getParam('id') !== undefined) {
+            this._editMode = 'edit';
+            this._recipeIndex = +this._routeSegment.getParam('id');
+        }
+        let fbRecipeName = '';
+        let fbRecipleImageUrl = '';
+        let fbRecipeContent = '';
+        let fbIngredients:ControlArray = new ControlArray([]);
+
+        if (this._editMode === 'edit') {
+            this.recipe = this._recipeService.getRecipe(this._recipeIndex);
+            for (let i = 0; i < this.recipe.ingredients.length; i++) {
+                fbIngredients.push(
+                    new ControlGroup(
+                        {
+                            name: new Control(this.recipe.ingredients[i].name, Validators.required),
+                            amount: new Control(this.recipe.ingredients[i].amount, Validators.compose([
+                                Validators.required,
+                                hasNumbers,
+                                greaterZero
+                            ]))
+                        }
+                    )
+                );
+                fbRecipeName = this.recipe.name;
+                fbRecipleImageUrl = this.recipe.imageUrl;
+                fbRecipeContent = this.recipe.content;
+            }
+        }
+        this.myForm = this._formBuilder.group({
+            name: [fbRecipeName, Validators.required],
+            imageUrl: [fbRecipleImageUrl],
+            content: [fbRecipeContent],
+            ingredients: this._formBuilder.array(fbIngredients.controls)
+        });
+    }
+
+
+
+
+}
+
+function hasNumbers(control:Control):{[s: string]: boolean} {
+    if (!('' + control.value).match('\\d+')) {
+        return {noNumbers: true};
+    }
+}
+
+function greaterZero(control:Control):{[s: string]: boolean} {
+    if (!((+control.value) > 0)) {
+        return {tooSmall: true};
+    }
 }
